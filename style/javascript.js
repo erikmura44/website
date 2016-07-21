3ad0f6ed7f525e8cc418db54a463c4c9c03eff8a
erikmura44 <erikmura44@gmail.com>
2016-07-21 13:25:21 -0600

    added contact form and javascript

--- style/javascript.js
+++ style/javascript.js
@@ -1,1 +1,11 @@
 
+$("input[type=text], input[type=email], #msg").on("click", function (event) {
+    var color = $(this).val();
+    $("form").css("background-color", "#beddf3");
+});
+$("button[type=submit]").hover(function (event) {
+    $("form").css("background-color", "white");
+});
+$("button[type=submit]").on("click", function (event) {
+    $("form").css("background-color", "#83F52C");
+});
